465889e3aff0949ab4863ff6210cbf3226fbe5c3
rick magno <rickmagno@github.com>
2020-04-14 21:12:06 -0300

    primeiro commit

---
+++ components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div className='py-4 text-center'>
+        MyDailyStatus é um projeto criado durante o Fullstack Lab do DevPleno e 
+        implementado por: <a href='https://www.linkedin.com/in/ricardo-magno-moreira-364585169/'> Rick Magno</a>
+        </div>
+    ) 
+}
+export default Footer
---
+++ components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import NavBar from './NavBar'
+
+const Header = () => {
+    return(
+        <div className='bg-gray-300'>
+        <h1>
+        <img className='h-24 mx-auto py-4'
+         src='/logo.png' 
+         height='60'
+         />
+        </h1>
+       <NavBar />
+        </div>
+    )
+}
+export default Header
